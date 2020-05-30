631757ed12120606e2a837b9211e1be5e152cc80
Dongyeong Kim <42883224+Dongyeongkim@users.noreply.github.com>
2020-05-30 15:54:34 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,29 @@
+import ray
 import model
-import tensorflow as tf
+import ray.rllib.agents.a3c as a3c
+from ray.tune.logger import pretty_print
+from ray.rllib.models import ModelCatalog
+
+ray.init(num_gpus=2)
+config = a3c.DEFAULT_CONFIG.copy()
+VirtLocalCDC= model.EpiNN()
+ModelCatalog.register_custom_model("CDC_model", VirtLocalCDC)
+
+config["num_gpus"] = 2
+config["num_workers"] = 10
+config["eager"] = True
+config["model"] = "CDC_model"
+
+
+trainer = a3c.A3CTrainer(config=config,env=)
+
+for i in range(1000):
+   result = trainer.train()
+   print(pretty_print(result))
+
+   if i % 100 == 0:
+       checkpoint = trainer.save()
+       print("checkpoint saved at", checkpoint)
+
+
+
